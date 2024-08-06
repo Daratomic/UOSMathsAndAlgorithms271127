0918e105e586c5ebab70eb814f796c20e669de80
Rob Stevens <robertlong214@gmail.com>
2024-08-06 23:28:29 +0100

    Finished Edits and Assignment Resub

--- Maths and Algorithms/Algorithms.cpp
+++ Maths and Algorithms/Algorithms.cpp
@@ -1,5 +1,12 @@
 #include "Algorithms.h"
 
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
 int* Algorithms::BubbleSort(int* Data, size_t Size)
 {
 
@@ -80,9 +87,75 @@ int* Algorithms::SelectionSort(int* Data, size_t Size)
     return copyArr;
 }
 
+int getParent(int child) // get the parent of an element
+{
+    return (child - 1) / 2;
+}
+
+int getLeftChild(int parent) // get the left child of the parent
+{
+    return (parent * 2) + 1;
+}
+
+int getRightChild(int parent) // get the right child of the parent
+{
+    return (parent * 2) + 2;
+}
+
+void siftdown(int* Data, int root, int end)
+{
+    while (getLeftChild(root) < end)
+    {
+        int child = getLeftChild(root);
+
+        if (getRightChild(root) < end && Data[getLeftChild(root)] > Data[getRightChild(root)]) // select the smaller of the two children
+        {
+            child = getRightChild(root);
+        }
+
+        if (Data[root] > Data[child]) //  if root is larger than the child element, swap them and continue
+        {
+            swap(Data[root], Data[child]);
+            root = child;
+        }
+        else
+        {
+            return;
+        }
+    }
+}
+
+void heapify(int* Data, int count) 
+{
+    int start = getParent(count - 1) + 1;
+
+    while (start > 0)
+    {
+        start--;
+
+        siftdown(Data, start, count);
+    }
+}
+
 int* Algorithms::HeapSort(int* Data, size_t Size)
 {
-    return nullptr;
+    heapify(Data, Size);
+
+    size_t end = Size;
+    while (end > 1)
+    {
+        end--;
+        swap(Data[end], Data[0]);
+        siftdown(Data, 0, Size);
+    }
+
+    int* copyArr = new int[Size];
+    for (int i = 0; i < Size; ++i)
+    {
+        copyArr[i] = Data[i];
+    }
+
+    return copyArr;
 }
 
 void Merge(int* data, size_t start, size_t midpoint, size_t end)
@@ -159,9 +232,50 @@ int* Algorithms::MergeSort(int* Data, size_t Size)
     return copyArr;
 }
 
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low; // temp pivot index
+
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            swap(arr[i], arr[j]);
+            i++;
+        }
+    }
+
+    swap(arr[i], arr[high]);
+    return i;
+}
+
+void quicksort_recursion(int* Data, size_t begin, size_t end)
+{
+    if (begin < end) // only apply recursion if beginning is bigger than the end index
+    {
+        int pivot_index = partition(Data, begin, end); // where the split/partition will occur
+
+        if (pivot_index > 0)
+        {
+            quicksort_recursion(Data, begin, pivot_index - 1); // left side of the partition
+        }
+
+        quicksort_recursion(Data, pivot_index + 1, end); // right side of the partition
+    }
+}
+
 int* Algorithms::QuickSort(int* Data, size_t Size)
 {
-    return nullptr;
+    int* copyArr = new int[Size];
+    for (int i = 0; i < Size; ++i)
+    {
+        copyArr[i] = Data[i];
+    }
+    
+    quicksort_recursion(copyArr, 0, Size - 1);
+
+    return copyArr;
 }
 
 int* Algorithms::BinSort(int* Data, size_t Size)
@@ -171,7 +285,13 @@ int* Algorithms::BinSort(int* Data, size_t Size)
 
 int* Algorithms::LinierSearch(int* Data, size_t Size, int target)
 {
-    return nullptr;
+    for (int i = 0; i < Size; i++) // loop through array
+    {
+        if (Data[i] == target) // if the declared element is found, return it
+        {
+            return &Data[i];
+        }
+    }
 }
 
 int* Algorithms::BinarySearch(int* Data, size_t Size, int target)
